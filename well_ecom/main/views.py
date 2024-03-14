ef046d91c5555459ab13a02f8502467502787f58
depo_ <propopa228@mail.ru>
2024-03-14 12:31:13 +0800

    refactor: shorten the amount of code in index page, make context directory in views

--- well_ecom/main/views.py
+++ well_ecom/main/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
 
 positions = [
-    {'id': 1, 'name': 'Резцы',' subname_1': 'THE FIRST NAME', 'subname_2': 'THE SECOND NAME', 'quan_1': 'FIRST QUAN', 'quan_2': 'SECOND QUAN'},
+    {'id': 1, 'name': 'Резцы', 'subname_1': 'THE FIRST NAME', 'subname_2': 'THE SECOND NAME', 'quan_1': 'FIRST QUAN', 'quan_2': 'SECOND QUAN'},
     {'id': 2, 'name': 'Зубья', 'subname_1': 'THE FIRST NAME', 'subname_2': 'THE SECOND NAME', 'quan_1': 'FIRST QUAN', 'quan_2': 'SECOND QUAN'},
-    {'id': 3, 'name': 'Болты', 'subname_1': 'THE FIRST NAME', 'subname_2': 'THE SECOND NAME', 'quan_1': 'FIRST QUAN', 'quan_2': 'SECOND QUAN'},
-
+    {'id': 3, 'name': 'АВОЫШЛАОлЫВОАЛОЫА', 'subname_1': 'THE FIRST NAME', 'subname_2': 'THE SECOND NAME', 'quan_1': 'FIRST QUAN', 'quan_2': 'SECOND QUAN'},
+{'id': 4, 'name': 'Гусеницы', 'subname_1': 'THE FIRST NAME', 'subname_2': 'THE SECOND NAME', 'quan_1': 'FIRST QUAN', 'quan_2': 'SECOND QUAN'},
+{'id': 5, 'name': 'Пружины', 'subname_1': 'THE FIRST NAME', 'subname_2': 'THE SECOND NAME', 'quan_1': 'FIRST QUAN', 'quan_2': 'SECOND QUAN'},
+{'id': 6, 'name': 'Пильные диски', 'subname_1': 'THE FIRST NAME', 'subname_2': 'THE SECOND NAME', 'quan_1': 'FIRST QUAN', 'quan_2': 'SECOND QUAN'},
 ]
 
 
+
+
+
+
 def home(request):
-    context = {'positions: positions'}
+    context = {'positions': positions}
     return render(request, 'index.html',context)
 
 def catalog(request):
