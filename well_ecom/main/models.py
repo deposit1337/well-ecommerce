1bb1aa768425e9e91535eae0bd38caaaee26f55a
depo_ <propopa228@mail.ru>
2024-03-15 19:36:15 +0800

    feat: add slug and connection between category and items

--- well_ecom/main/models.py
+++ well_ecom/main/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 # Create your models here.
+# from django.utils.text import slugify
+
+from django.db import models
+from django.utils.text import slugify
 
 
 # Categories of products
@@ -12,11 +16,19 @@ class Category(models.Model):
     quantity_1 = models.IntegerField(default=1)
     quantity_2 = models.IntegerField(default=1)
 
-    image = models.ImageField(upload_to='uploads/product/',default='well_ecom/media/uploads/product/default.png')
+    image = models.ImageField(upload_to='uploads/product/',default='uploads/product/defualt.png')
+    slug = models.SlugField(max_length=100, unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name_plural = 'Categories'
 
 class Product(models.Model):
     brand_name = models.CharField(max_length=50)
--- well_ecom/main/views.py
+++ well_ecom/main/views.py
@@ -13,11 +13,27 @@ from django.shortcuts import render
 # {'id': 6, 'name': 'Пильные диски', 'subname_1': 'THE FIRST NAME', 'subname_2': 'THE SECOND NAME', 'quan_1': 'FIRST QUAN', 'quan_2': 'SECOND QUAN'},
 # ]
 
-
-
+from .models import Category,Product
 
 
 
 def home(request):
-    return render(request, 'index.html')
+    categories_red = Category.objects.all()[:3]
+    categories_grey = Category.objects.all()[3:6]
 
+    return render(request, 'index.html',{'categories_red': categories_red,'categories_grey':categories_grey})
+
+# def products(request):
+#     products = Product.objects.all()
+#     return render(request,'catalog.html',{'products':products})
+
+
+from django.http import Http404
+
+from django.utils.translation import gettext as _
+from django.shortcuts import get_object_or_404
+
+def category_products(request, category_slug):
+    category = get_object_or_404(Category, slug=category_slug)
+    products = Product.objects.filter(category=category)
+    return render(request, 'catalog.html', {'category': category, 'products': products})
